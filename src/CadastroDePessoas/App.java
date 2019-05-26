79535c1db0d80d40e4de408f38d4420fa659fa48
danimiki <39574030+danimiki@users.noreply.github.com>
2019-05-26 18:02:17 -0300

    Update App.java

--- src/CadastroDePessoas/App.java
+++ src/CadastroDePessoas/App.java
@@ -4,26 +4,19 @@ public class App {
 	
 	public static void main(String[] args) {
 		
-		Fisica pessoa1 = new Fisica("daniela", "12345678909", "987678905");
+		Juridica Unicesumar = new Juridica(new Nome("Unicesumar Centro Univercitário Cesumar Ltda."), new Cnpj("02.004.713/0001-55"), 
+									   new CapitalSocial(500_000d));
 		
-		System.out.println("id: " + pessoa1.getId() + ", nome: " + pessoa1.getNome() + ", rg: " +
-		pessoa1.getRg() + ", cpf: " + pessoa1.getCpf());
+		Fisica Daniela = new Fisica(new Nome("Daniela Miki"),new Cpf("12345678909"), new Rg("987678905"));
 		
-		Juridica pessoaJur1 = new Juridica("Alan Walker", "0923647589208", 500);
-		Juridica pessoaJur2 = new Juridica("Martin Garrix", "019283746576", 250);
+		Fisica Lucas = new Fisica(new Nome("Lucas Hiroshi"), new Cpf("2255346789"), new Rg("1133446628"));
+	
+		Unicesumar.adicionarSocio(Lucas, new PercentualDeParticipacao(25));
 		
-		Fisica pessoa2 = new Fisica("Lucas Hiroshi", "2255346789", "1133446628");
-		Fisica pessoa3 = new Fisica("Luiza Yumi", "324465452", "21225654356");
-
-		pessoaJur1.adicionarSocio(pessoa1, 40);
+		Unicesumar.removerSocio(Lucas);
 		
-		pessoaJur1.adicionarSocio(pessoa2, 20);
+		Unicesumar.adicionarSocio(Daniela, new PercentualDeParticipacao(80));
 		
-		pessoaJur1.adicionarSocio(pessoaJur2, 25);
-
-		pessoaJur1.adicionarSocio(pessoaJur1, 80);
-		
-		pessoaJur1.removerSocio(pessoa3);
 		
 	}
 
