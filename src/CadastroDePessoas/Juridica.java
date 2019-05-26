e643a26fd6216fb6af94ec5babbfecb1ed8424a3
danimiki <39574030+danimiki@users.noreply.github.com>
2019-05-26 17:46:08 -0300

    Update Juridica.java

--- src/CadastroDePessoas/Juridica.java
+++ src/CadastroDePessoas/Juridica.java
@@ -2,73 +2,96 @@ package CadastroDePessoas;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
 
 public class Juridica extends Pessoa {
+	private Cnpj cnpj;
+	private CapitalSocial capitalSocial;
+	private Set<CotaSociedade> cotasSociedade = new HashSet<>();
 	
-	private String cnpj;
-	private double capitalSocial;
-	private Set<CotaSociedade> cotasSociedade = new HashSet<CotaSociedade>();
-	
-	public Juridica(String nome, String cnpj, double capitalSocial) {
+	public Juridica(Nome nome, Cnpj cnpj, capitalSocial capitalSocial) {
 		super(nome);
 		this.capitalSocial = capitalSocial;
 		this.cnpj = cnpj;
 	}
 	
-	public Juridica(UUID id, String nome, String cnpj, double capitalSocial) {
+	public Juridica(Id id, NOme nome, Cnpj cnpj, capitalSocial capitalSocial) {
 		super(id, nome);
 		this.capitalSocial = capitalSocial;
 		this.cnpj = cnpj;
 	}
 	
+	public void adicionarSocio(Pessoa socio, percentualDeParticipacao percentualDeParticipacao) {
+		CotaSociedade novaCota = new CotaSociedade();
+		novaCota.socio = socio;
+		novaCota.percentualDeParticipacao = percentualDeParticipacao;
+		
+		double percentualAtual = somarPercentualAtual();
+		if (percentualAtual + percentualDeParticipacao.imprimePercentualDeParticipacao()> 100.00) {
+			throw new RuntimeException("A participação total não pode exceder 100%! Percentual atual: " + percentualAtual + ". Você tentou adicionar mais " + percentualDeParticipacao.imprimePercentualDeParticipacao());
+		}
+		
+		this.cotasSociedade.add(novaCota);
+	}
+	private double somarPercentualAtual() {
+		double percentualAtual = 0.00d;
+		for (CotaSociedade cotaSociedade : cotasSociedade) {
+			percentualAtual += cotaSociedade.percentualDeParticipacao.imprimePercentualDeParticipacao();
+		}
+		return percentualAtual;
+	}
+	
+	public void removerSocio(Pessoa socioParaRemover) {
+		Set<CotaSociedade> aux = new HashSet<>();
+		for (CotaSociedade cota : cotasSociedade) {
+			if (!cota.socio.equals(socioParaRemover)) {
+				aux.add(cota);
+			}
+		}
+		this.cotasSociedade = aux;
+		
+	}
+	
 	public double getCapitalSocial() {
-		return capitalSocial;
+		return capitalSocial.imprimeCapitalSocial();;
 	}
 	public String getCnpj() {
-		return cnpj;
+		return cnpj.imprimeCapitalSocial();
 	}
-	
-	public void adicionarSocio(Pessoa socio, double percentualDeParticipacao) {
-		
-		if (socio.id != this.id) {
-			
-			cotasSociedade.add(new CotaSociedade(socio, percentualDeParticipacao));
-		} else {
-			
-			System.out.println("nao pode ser socio de si mesmo!");
-		}
-	}
-	
-	public void removerSocio(Pessoa socio) {
-		
-		boolean verificacao = false;
-		for (CotaSociedade cota : cotasSociedade) {
-			
-			if(cota.socio == socio) {
-				
-				cotasSociedade.remove(cota);
-				verificacao = true;
-			}		
-		}
-		
-		if (!verificacao) {
-			
-			System.out.println(socio.getNome() + " nao eh socio!");
-		}
-		
-	}
-	
 	
 	private class CotaSociedade{
-		
-		private double percentualDeParticipacao;
+		private PercentualDeParticipacao percentualDeParticipacao;
 		private Pessoa socio;
 		
-		public CotaSociedade(Pessoa socio, double percentualDEPArticipacao) {
-			
-			this.percentualDeParticipacao = percentualDEPArticipacao;
-			this.socio = socio;
+		
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getOuterType().hashCode();
+			result = prime * result + ((socio == null) ? 0 : socio.hashCode());
+			return result;
 		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			CotaSociedade other = (CotaSociedade) obj;
+			if (!getOuterType().equals(other.getOuterType()))
+				return false;
+			if (socio == null) {
+				if (other.socio != null)
+					return false;
+			} else if (!socio.equals(other.socio))
+				return false;
+			return true;
+		}
+		private Juridica getOuterType() {
+			return Juridica.this;
+		}
+		
 	}
 }
